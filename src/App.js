d486476dec4e7be3c57b2451d3a4e9d129a1ad53
amlan2000 <amlanmishra0911@gmail.com>
2024-08-31 19:03:38 +0530

    basic google maps component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const MapComponent = () => {
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: 'AIzaSyCH5Y_nerXn_ssQ8SvG3mz3IcOKKh9amS0' });
+  const mapRef = React.useRef();
+
+  const onLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const captureMap = () => {
+    // Function to capture the map as an image
+  };
+
+  return isLoaded ? (
+    <GoogleMap
+      onLoad={onLoad}
+      mapContainerStyle={{ width: '400px', height: '400px' }}
+      center={{ lat: -34.397, lng: 150.644 }}
+      zoom={8}
+    >
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      <button onClick={captureMap}>Capture Map</button>
+    </GoogleMap>
+  ) : <div>Loading...</div>;
+};
+
+export default MapComponent;
