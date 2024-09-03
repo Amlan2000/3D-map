bca155cae37ffa8c05be29752ad11da485fdb68e
amlan2000 <amlanmishra0911@gmail.com>
2024-09-03 17:55:26 +0530

    client url

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "https://lighthearted-bombolone-e9067a.netlify.app",
+    origin: process.env.CLIENT_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
