51e8d989affa0258addd4554ae65be9d116cf6c9
amlan2000 <amlanmishra0911@gmail.com>
2024-09-10 14:02:59 +0530

    new SSO, user able to sign up

---
+++ backend/Routes/signup.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const signUpController = require("../controllers/signup");
+
+
+const router= express.Router();
+
+router.post("/register", signUpController.createUser);
+
+module.exports=router;
