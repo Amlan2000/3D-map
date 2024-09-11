ad1e9354f07c272f80910091050741ad06d58f49
amlan2000 <amlanmishra0911@gmail.com>
2024-09-11 17:49:00 +0530

    cors settings

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,34 @@
 require('dotenv').config();
 require('./redisClient');
 const express = require('express');
-const signUpRoute= require("./Routes/signupRoutes");
-const bodyParser= require("body-parser")
+const signUpRoute = require("./Routes/signupRoutes");
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const cors = require('cors');
-const loginRoute = require("./Routes/loginRoutes")
-const userRoute= require("./Routes/userRoutes")
+const loginRoute = require("./Routes/loginRoutes");
+const userRoute = require("./Routes/userRoutes");
 const mapCaptureRoutes = require('./Routes/mapCaptureRoutes');
-
 
 const uri = process.env.MONGODB_URI || "your_mongo_uri_here";
 mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Failed to connect to MongoDB Atlas', err));
 
-const app =express();
+const app = express();
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL, // Your frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
-
-app.use("/user",signUpRoute);
-app.use("/auth",loginRoute);
-app.use("/api",userRoute);
+app.use("/user", signUpRoute);
+app.use("/auth", loginRoute);
+app.use("/api", userRoute);
 app.use("/map", mapCaptureRoutes);
-
-
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
